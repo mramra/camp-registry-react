6682103be60fd65bdd9aa59f68a1d25273d0db67
Eng. Mahmoud Ramadan <mramra1980@gmail.com>
2026-06-09 15:06:08 +0300

    add src/pages/SMS/SMS.jsx

---
+++ src/pages/SMS/SMS.jsx
@@ -0,0 +1,12 @@
+import PageHeader from '../../components/ui/PageHeader'
+import Card from '../../components/ui/Card'
+import EmptyState from '../../components/ui/EmptyState'
+
+export default function SMS() {
+  return (
+    <div>
+      <PageHeader title="SMS" />
+      <Card><EmptyState title="قيد التطوير" /></Card>
+    </div>
+  )
+}
