9d653b5ac77627152794e742ce99805b39990a6b
Eng. Mahmoud Ramadan <mramra1980@gmail.com>
2026-06-09 15:06:20 +0300

    add src/pages/Distributions/Distributions.jsx

---
+++ src/pages/Distributions/Distributions.jsx
@@ -0,0 +1,12 @@
+import PageHeader from '../../components/ui/PageHeader'
+import Card from '../../components/ui/Card'
+import EmptyState from '../../components/ui/EmptyState'
+
+export default function Distributions() {
+  return (
+    <div>
+      <PageHeader title="Distributions" />
+      <Card><EmptyState title="قيد التطوير" /></Card>
+    </div>
+  )
+}
