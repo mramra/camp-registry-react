c894da3f18fb877ed795ec87572da8b949ff8dcb
Eng. Mahmoud Ramadan <mramra1980@gmail.com>
2026-06-15 00:47:45 +0300

    fix: force SW update on load to clear stale cache

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// مسح الـ Service Worker القديم وإجبار التحديث
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    // إلغاء تسجيل SW القديمة
+    regs.forEach(reg => {
+      reg.update()
+      if (reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' })
+    })
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
