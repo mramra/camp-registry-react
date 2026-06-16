1c7b532afc7b2d8ecf506adb2df98cffbd417598
Eng. Mahmoud Ramadan <mramra1980@gmail.com>
2026-06-16 10:35:54 +0300

    feat: controllers barrel export

---
+++ src/controllers/index.js
@@ -0,0 +1,7 @@
+/**
+ * controllers/index.js — تصدير مركزي
+ */
+export { FamilyController }      from './FamilyController'
+export { CampController }        from './CampController'
+export { UserController }        from './UserController'
+export { DistributionController } from './DistributionController'
